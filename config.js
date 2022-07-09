2afcdf5dc061686028bd08307b8e22cc5ff1e6dd
holybaechu <66154521+baechooYT@users.noreply.github.com>
2022-07-09 21:13:49 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     roblosecurity: '', // Without roblosecurity, it will throw Too Many Requests error but you can increase the interval
     interval: 600, // Interval in milliseconds (600 recommended)
-    port: 433, // Port to listen on
+    port: process.env.PORT || 8080, // Port to listen on
 }
